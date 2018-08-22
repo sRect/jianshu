94a78edfb80f517eae23bbd7ca3d9d8fde7773b6
srect <fcqun@sina.cn>
2018-08-22 23:42:39 +0800

    :sparkles: feat: 使用styled-components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './indexStyle';
+import 'reset-css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
---
+++ src/indexStyle.js
@@ -0,0 +1,24 @@
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+body, html {
+  font-size: 14px;
+  color: #333;
+}
+.fl{
+	float: left;
+}
+
+.fr{
+	float: right;
+}
+
+.clearfix:after { content: ''; display: block; height: 0; clear: both; }
+.clearfix { zoom: 1; }
+
+// 连续英文、数字换行
+.wordwrap { word-break: break-all; word-wrap: break-word; }
+// 单行文字超出显示省略号
+.omg{overflow: hidden; text-overflow: ellipsis; white-space:nowrap;}
+`
+
