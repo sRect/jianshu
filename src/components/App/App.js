33d8f98c973ea164ea035df8b640d193d5b6e8e7
srect <fcqun@sina.cn>
2018-08-22 23:49:13 +0800

    :construction: feat: 创建Header组建

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hello world</h1>
+        <Header></Header>
       </div>
     );
   }
