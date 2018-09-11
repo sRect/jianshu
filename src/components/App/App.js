3d68f818a979db29d6aa7a6f804ac41e7c099896
srect <fcqun@sina.cn>
2018-09-11 22:29:59 +0800

    :sparkles: feat: 使用react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import Header from '../Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header></Header>
+        <Header />
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact render={() => <div>home</div>}></Route>
+            <Route path="/detail" exact render={() => <div>detail</div>}></Route>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
