5421648ec352310afebbef0250b3adbc05b66048
srect <fcqun@sina.cn>
2018-09-17 00:15:18 +0800

    :lipstick: feat: 完成对List组件的布局

--- src/components/Home/List.js
+++ src/components/Home/List.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { ListWrapper, ListItem } from './style'
 
 class List extends Component {
   render() {
     return (
-      <div>List</div>
+      <ListWrapper>
+        <ListItem>
+          <img src="//upload-images.jianshu.io/upload_images/9807853-11fe67ba82dc2ed9.png?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240" alt="img" />
+          <div className="content">
+            <h4 className="title">有那种网站吗，嗯？分享5个好用的在线工具！</h4>
+            <p className="abstract">工作中，我们常常有一些很小需求，比如传图的时候图片太大，需要压缩一下。 而往往专业的工具动辄几百M，下载安装仅仅是用其中的一个小功能，实在不值得...</p>
+          </div>
+        </ListItem>
+      </ListWrapper>
     )
   }
 }
--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -48,3 +48,42 @@ export const TopicItem = styled.li`
     text-align: center;
   }
 `
+
+export const ListWrapper = styled.ol`
+  width: 100%;
+  height: auto;
+`
+
+export const ListItem = styled.li`
+  position: relative;
+  width: 100%;
+  margin: 0 0 15px;
+  padding: 15px 2px 20px 0;
+  border-bottom: 1px solid #f0f0f0;
+  word-wrap: break-word;
+  cursor: pointer;
+  > img {
+    position: absolute;
+    top: 50%;
+    margin-top: -60px;
+    right: 0;
+    width: 125px;
+    height: 100px;
+  }
+  > .content {
+    padding-right: 140px;
+    > h4 {
+      margin: -7px 0 4px;
+      display: inherit;
+      font-size: 18px;
+      font-weight: 700;
+      line-height: 1.5;
+    }
+    > .abstract {
+      margin: 0 0 8px;
+      font-size: 13px;
+      line-height: 24px;
+      color: #999;
+    }
+  }
+`
