173c77a4891988a0452eed1279dee7a7fbea9de1
srect <fcqun@sina.cn>
2018-09-16 22:50:15 +0800

    :lipstick: feat: 完成Topic组件布局

--- src/components/Home/Topic.js
+++ src/components/Home/Topic.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { TopicWrapper, TopicItem } from './style'
 
 class Topic extends Component {
   render() {
     return (
-      <div>Topic</div>
+      <TopicWrapper>
+        <TopicItem>
+          <img src="http://upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64" alt="img" />
+          <div className="name">
+            简书电影
+          </div>
+        </TopicItem>
+      </TopicWrapper>
     )
   }
 }
--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -19,3 +19,32 @@ export const HomeRight = styled.div`
   width: 270px;
   
 `
+
+export const TopicWrapper = styled.ul`
+  width: 100%;
+  margin: 35px 0 20px 0;
+  border-bottom: 1px solid #dcdcdc;
+`
+
+export const TopicItem = styled.li`
+  display: inline-block;
+  margin: 0 18px 18px 0;
+  min-height: 32px;
+  background-color: #f7f7f7;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
+  vertical-align: top;
+  overflow: hidden;
+  cursor: pointer;
+  > img {
+    width: 32px;
+    height: 32px;
+    vertical-align: middle;
+  }
+  > .name {
+    font-size: 14px;
+    display: inline-block;
+    padding: 0 11px 0 6px;
+    text-align: center;
+  }
+`
